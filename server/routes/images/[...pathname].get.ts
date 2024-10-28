afd6421a45d0f371f3dbfe2d39c33442820c1f42
David <67317883+GreenmeisterDavid@users.noreply.github.com>
2024-10-28 11:26:04 +0100

    fix: fallback to github login if name is not available (#23)

--- server/routes/images/[...pathname].get.ts
+++ server/routes/images/[...pathname].get.ts
@@ -1,5 +1,12 @@
 export default eventHandler(async (event) => {
   const { pathname } = getRouterParams(event)
 
+  if (!pathname) {
+    return createError({
+      statusCode: 404,
+      statusMessage: 'Not Found',
+    })
+  }
+
   return hubBlob().serve(event, pathname)
 })
