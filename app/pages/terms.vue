ae47ddfe49d89d26282e1ddbd7979e35d71f8331
barbapapazes <e.soubiran25@gmail.com>
2024-10-28 15:19:11 +0100

    feat: add terms and policy

--- app/pages/terms.vue
+++ app/pages/terms.vue
@@ -13,7 +13,128 @@ useSeoMeta({
         :title
       />
 
-      <!-- Use a UPageBody to build the user dashboard -->
+      <UPageBody>
+        <div class="prose prose-gray dark:prose-invert mx-auto max-w-none">
+          <p><em>Updated on 2024-10-28</em></p>
+
+          <p>Estéban Soubiran ("we", "us" or "our") agrees to furnish services to the Subscriber (the "Subscriber", "you" or "your"), subject to the following Terms of Service. Use of our service constitutes acceptance and agreement to our Terms of Service.</p>
+
+          <p>We reserve the right to modify the Terms of Service without notice.</p>
+
+          <h2
+            id="use-of-services"
+            tabindex="-1"
+          >
+            Use of Services <a
+              class="header-anchor"
+              href="#use-of-services"
+              aria-label="Permalink to &quot;Use of Services&quot;"
+            />
+          </h2>
+
+          <p>
+            You may use our services, provided that you are of legal age to form a binding contract and are not barred from receiving such services under the laws of France or other jurisdictions. In order to access our services, you are required to provide current and factual identification, contact, and other information as part of the registration process. You are responsible for the confidentiality of your account information and for all activities that occur under your account. You are solely responsible for all content within your account. You agree to immediately notify us (<a
+              href="mailto:esteban@soubiran.dev"
+              target="_blank"
+              rel="noreferrer"
+            >esteban@soubiran.dev</a>) of any unauthorized use of your account or any other breach of security. We will not be liable for any loss or damage as a result of your failure to provide us with accurate information or to keep your account secure.
+          </p>
+
+          <p>By using our services, you acknowledge and agree that we may use third-party analytics and tracking tools to collect and analyze information about your use of our website. This information is used to improve our services and user experience. For more information about the data collected and how it is used, please refer to our <a href="/privacy">Privacy Policy</a>.</p>
+
+          <h2
+            id="acceptable-conduct"
+            tabindex="-1"
+          >
+            Acceptable Conduct <a
+              class="header-anchor"
+              href="#acceptable-conduct"
+              aria-label="Permalink to &quot;Acceptable Conduct&quot;"
+            />
+          </h2><p>You are responsible for the actions of all users of your account and any data that is created, stored, displayed by, or transmitted by your account while using our services. You will not engage in any activity that interferes with or disrupts our services or networks connected to us.</p><h2
+            id="prohibited-usage"
+            tabindex="-1"
+          >
+            Prohibited Usage <a
+              class="header-anchor"
+              href="#prohibited-usage"
+              aria-label="Permalink to &quot;Prohibited Usage&quot;"
+            />
+          </h2>
+          <p>You agree that any of the below activities are considered prohibited usage and will result in immediate account suspension the possibility that we will pursue civil remedies without providing advance notice.</p>
+
+          <p>Misuse of System Resources: Intentional misuse of system resources, including but not limited to employing programs that consume excessive network capacity, CPU cycles, or disk IO.</p>
+
+          <p>Access to Other Computers or Networks without Authorization: Attempting unauthorized and/or illegal access of computers, networks and/or accounts not belonging to party seeking access. Any act which interferes with the services of another user or network. Any act relating to the circumvention of security measures.</p>
+
+          <p>The services we provides may only be used for lawful purposes. Transmission, distribution, or storage of any information, data or material in violation of France laws, or by the common law, is prohibited. This includes, but is not limited to, material protected by copyright, trademark, trade secret, or other intellectual property rights. Our services may not be used to facilitate infringement of these laws in any way.</p>
+
+          <h2
+            id="account-cancellation-or-suspension"
+            tabindex="-1"
+          >
+            Account Cancellation or Suspension <a
+              class="header-anchor"
+              href="#account-cancellation-or-suspension"
+              aria-label="Permalink to &quot;Account Cancellation or Suspension&quot;"
+            />
+          </h2>
+
+          <p>We reserve the right to suspend network access to any customer if, in the judgment of our network administrators, the customer’s server is the source or target of a violation of any of the other terms of service or for any other reason which we choose. We will use reasonable care in notifying the Customer and in resolving the problem in a method resulting in the least amount of service interference. We reserve the right to terminate service without notice for continued and repeated violations of the terms of service.</p>
+
+          <h2
+            id="disclosure-to-law-enforcement"
+            tabindex="-1"
+          >
+            Disclosure to Law Enforcement <a
+              class="header-anchor"
+              href="#disclosure-to-law-enforcement"
+              aria-label="Permalink to &quot;Disclosure to Law Enforcement&quot;"
+            />
+          </h2>
+
+          <p>The Terms of Service specifically prohibits the use of our service for illegal activities. Therefore, Subscriber agrees that we may disclose any and all subscriber information including assigned IP numbers, account history, account use, etc. to any legal authority that requests such information without further consent or notification to the Subscriber, in compliance with French law and European Union regulations.</p>
+
+          <h2
+            id="governing-law"
+            tabindex="-1"
+          >
+            Governing Law <a
+              class="header-anchor"
+              href="#governing-law"
+              aria-label="Permalink to &quot;Governing Law&quot;"
+            />
+          </h2>
+
+          <p>These Terms of Service and the relationship between you and us shall be governed by the laws of France without regard to its conflict of law provisions. You and us agree to submit to the personal and exclusive jurisdiction of the courts located within France.</p>
+
+          <h2
+            id="severability"
+            tabindex="-1"
+          >
+            Severability <a
+              class="header-anchor"
+              href="#severability"
+              aria-label="Permalink to &quot;Severability&quot;"
+            />
+          </h2>
+
+          <p>If any provision of these Terms of Service is found by a court of competent jurisdiction to be invalid, the parties nevertheless agree that the court should endeavor to give effect to the parties’ intentions as reflected in the provision, and the other provisions of the Terms of Service remain in full force and effect.</p>
+
+          <h2
+            id="entire-agreement"
+            tabindex="-1"
+          >
+            Entire Agreement <a
+              class="header-anchor"
+              href="#entire-agreement"
+              aria-label="Permalink to &quot;Entire Agreement&quot;"
+            />
+          </h2>
+
+          <p>These Terms of Service constitute the entire agreement between you and us and govern your use of the us services, superseding any prior agreements between you and us for the use of our services.</p>
+        </div>
+      </UPageBody>
     </UPage>
   </UContainer>
 </template>
